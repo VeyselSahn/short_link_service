80dd523e3d7f1e6ef1295da50230ecd3e6771e61
VeyselSahn <79803069+VeyselSahn@users.noreply.github.com>
2022-05-24 16:54:07 +0300

    Update shorted_links.dart

--- bin/links/shorted_links.dart
+++ bin/links/shorted_links.dart
@@ -2,4 +2,4 @@ import '../model/link_model.dart';
 
 List<LinkModel> shortedLinks = [basic];
 var basic =
-    LinkModel(code: 'test', originalLink: 'https://grisoftbilisim.com.tr', shortLink: 'http://localhost:8080/test');
+    LinkModel(code: 'test', originalLink: 'https://google.com.tr', shortLink: 'http://localhost:8080/test');
